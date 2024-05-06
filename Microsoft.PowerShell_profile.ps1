83f5430237a9bffe7f039e77e5314885e31c5e4d
Andreas Bradahl <andreasbradahl@gmail.com>
2024-05-06 15:53:25 +0200

    update powershell profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,5 @@
-Import-Module posh-git
+﻿Import-Module posh-git
+Set-Alias ssh-agent "$env:ProgramFiles\git\usr\bin\ssh-agent.exe"
+Set-Alias ssh-add "$env:ProgramFiles\git\usr\bin\ssh-add.exe"           
+$GitPromptSettings.DefaultPromptAbbreviateHomeDirectory = $true         # remove user directory from prompt
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete                # display autocomplete menu
